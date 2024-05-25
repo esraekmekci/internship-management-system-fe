07808c1b8674e2f8b1ea24832feb4166655b82a4
Esra Ekmekci <esraekmekci11@gmail.com>
2024-05-25 22:23:39 +0300

    secretary loading page

--- src/PagesV2/Secretary/SecretaryHome.jsx
+++ src/PagesV2/Secretary/SecretaryHome.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import "./SecretaryHome.css";
 import Header from "../../Components/Header";
