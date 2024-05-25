e1c6b3e3f3aba42e1347441dc50d847edcbfd618
Esra Ekmekci <esraekmekci11@gmail.com>
2024-05-25 17:15:37 +0300

    fix bugs

--- src/PagesV2/Coordinator/CoordinatorHomeV2.jsx
+++ src/PagesV2/Coordinator/CoordinatorHomeV2.jsx
@@ -21,56 +21,8 @@ Benimle çalışan kişi yaptıkça commentleri silmeyi unutma pls.
 */
 
 const CoordinatorHomeV2 = ({ children }) => {
-  var [currentUser, setCurrentUser] = useState({});
   const [loading, setLoading] = useState(true);
   const { setUser } = useUser();
-  
-  const [showDropdown, setShowDropdown] = useState({
-    btn1: false,
-    btn1_1: false,
-    btn1_2: false,
-    btn2: false,
-    btn2_1: false,
-    btn2_2: false,
-    btn3: false,
-    btn3_1: false,
-    btn3_2: false,
-    btn4: false,
-    userbtn: false,
-    userbtn_1: false,
-    userbtn_2: false,
-    userbtn_3: false,
-    userbtn_4: false,
-    userbtn_5: false,
-    homebtn: false,
-  });
-
-  const toggleDropdown = (btn) => {
-    setShowDropdown((prev) => ({ ...prev, [btn]: !prev[btn] }));
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await GetWithAuth(
-          "/coordinator/token/" + localStorage.getItem("tokenKey")
-        );
-        const result = await response.json();
-        setUser(result);
-      } catch (error) {
-        console.log(error);
-        console.log("User not found");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    const timeout = setTimeout(() => {
-      fetchData();
-    }, 1);
-
-    return () => clearTimeout(timeout);
-  }, []);
 
   if (loading) {
     return (
