519753c92c15e4d5d51f1826cdc58683be7a3e39
Esra Ekmekci <esraekmekci11@gmail.com>
2024-05-25 21:29:07 +0300

    coordinator loading page

--- src/PagesV2/Coordinator/GuidelinesV2.jsx
+++ src/PagesV2/Coordinator/GuidelinesV2.jsx
@@ -206,7 +206,7 @@ export default function Guidelines() {
                 <div className="file-buttons">
                   <button
                     className="delete-button iyte-bg"
-                    style={{color: "#07401a"}}
+                    style={{backgroundColor: "#07401a"}}
                     onClick={() => handleDownload()}
                   >
                     Download
