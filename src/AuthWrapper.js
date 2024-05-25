5b5787d62cfe4615f8486b1679e166e8d151926f
Esra Ekmekci <esraekmekci11@gmail.com>
2024-05-25 23:34:59 +0300

    login config

--- src/AuthWrapper.js
+++ src/AuthWrapper.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import App from "./App";
 import CoordinatorHomeV2 from "./PagesV2/Coordinator/CoordinatorHomeV2";
 import CompanyHomeV2 from "./PagesV2/Company/CompanyHomeV2";
@@ -6,25 +7,32 @@ import SecretaryHome from "./PagesV2/Secretary/SecretaryHome";
 import Home from "./PagesV2/Student/HomeV2";
 
 const AuthWrapper = () => {
+  const navigate = useNavigate();
   const token = localStorage.getItem('tokenKey');
   const role = localStorage.getItem('role');
 
-  if (!token) {
-    return <App />;
-  }
-
-  switch (role) {
-    case 'STUDENT':
-      return <Home />;
-    case 'COORDINATOR':
-      return <CoordinatorHomeV2 />;
-    case 'COMPANY':
-      return <CompanyHomeV2 />;
-    case 'SECRETARY':
-      return <SecretaryHome />;
-    default:
-      return <App />; // Eğer role bilinmiyorsa veya hiçbiri değilse, App bileşenine yönlendir.
-  }
+  useEffect(() => {
+    if (token) {
+      switch (role) {
+        case 'STUDENT':
+          navigate("/std");
+          break;
+        case 'COORDINATOR':
+          navigate("/coor");
+          break;
+        case 'COMPANY':
+          navigate("/comp");
+          break;
+        case 'SECRETARY':
+          navigate("/sec");
+          break;
+        default:
+          return <App />;
+      }
+    } else {
+        return <App />;
+    }
+  }, [navigate, token, role]); // token veya role değişirse bu useEffect tetiklenecek
 };
 
 export default AuthWrapper;
