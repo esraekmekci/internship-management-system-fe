6afd9e7fdea926457abf573302e845bf6e04fc6c
Esra Ekmekci <esraekmekci11@gmail.com>
2024-06-04 23:17:29 +0300

    student register fixed

--- src/PagesV2/Company/CompanyInternsV2.jsx
+++ src/PagesV2/Company/CompanyInternsV2.jsx
@@ -132,7 +132,6 @@ function CompanyInternsV2() {
         setSelectedFile(null);
         setFileName("Select file");
         setUploading(false);
-        window.location.reload();
       })
       .catch((err) => {
         console.error("Error occurred:", err);
